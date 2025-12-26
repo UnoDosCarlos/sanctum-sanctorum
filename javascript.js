ded45cf3eedfc36b5016a12ff3fd594150af0ea7
UnoDosCarlos <the.immortal.carlos@gmail.com>
2025-12-26 14:33:53 -0500

    Add book card format;added styling to webpage
    Added a button that adds book cards to the webpage and updated the style of the website
    to improve functionality and make the website user friendly.

--- javascript.js
+++ javascript.js
@@ -1,35 +1,102 @@
 const openFormBtn = document.querySelector('#openFormBtn');
 const submitBtn = document.querySelector('#submitBtn');
+const readBtn = document.querySelector('#readBtn');
 const addTomePopUp = document.querySelector('.addTomePopUp');
+const bottomSection = document.querySelector('.bottom-section');
+const inputTitle = document.querySelector('#title');
+const inputAuthor = document.querySelector('#author');
+const inputDescription = document.querySelector('#description');
+const displayTitle = document.querySelector('.display-title');
+const displayAuthor = document.querySelector('.display-author');
+const displayDescription = document.querySelector('.display-description');
+const tomeCard = document.querySelector('.tome-card');
+const tomeBtn = document.querySelector('#tomeBtn');
+
+
+let bookID;
 
 openFormBtn.addEventListener('click', () => {
     addTomePopUp.classList.add('visible');
     openFormBtn.disabled = true;
-    console.log(addTomePopUp.classList.contains('visible'));
 });
 
-submitBtn.addEventListener('click', () => {
+submitBtn.addEventListener('click', event => {
+    event.preventDefault();
+    if( inputTitle.value === '' || inputAuthor.value === '' || inputDescription.value === '') {
+        return;
+    } 
+    addTomeToLibrary();
+    inputTitle.value = null;
+    inputAuthor.value = null;
+    inputDescription.value = null;
     addTomePopUp.classList.remove('visible');
     openFormBtn.disabled = false;
 });
 
+readBtn.addEventListener('click', () => {
+
+    if(tomeCard.classList.contains('read')) {
+    tomeCard.classList.remove('read');
+    } else {
+    tomeCard.classList.add('read');
+    };
+    
+});
+
+
 
 const myLibrary = [];
 
-function Book(title, author,description) {
+function Tome(title, author,description) {
     this.title = title;
     this.author = author;
     this.description = description;
-    this.id = addId();
+  // this.id = addId();
 };
 
-Book.prototype.addId = function() {
-    crypto.randomUUID()
+Tome.prototype.addId = function() {
+    let tomeID = crypto.randomUUID()
+    return tomeID;
 };
 
 
 
 
-function addBookToLibrary () {
+function addTomeToLibrary () {
+    const title = inputTitle.value;
+    const author = inputAuthor.value;
+    const description = inputDescription.value;
+
+    const newTome = new Tome(title,author,description);
+    let tome = document.createElement('div');
+    const tomeTitle = document.createElement('h3');
+    const tomeAuthor = document.createElement('h4');
+    const tomeDesc = document.createElement('p');
+    const tomeBtn = document.createElement('button');
+
+    tomeTitle.textContent += newTome.title;
+    tomeAuthor.textContent += newTome.author;
+    tomeDesc.textContent += newTome.description;
+    tomeBtn.textContent = 'Read';
+    tomeBtn.id = 'tomeBtn';
+
+    tome.classList.add('tome-card');
+    tome.classList.add('visible');
+    bottomSection.appendChild(tome);
+    tome.appendChild(tomeTitle);
+    tome.appendChild(tomeAuthor);
+    tome.appendChild(tomeDesc);
+    tome.appendChild(tomeBtn);
+
+    tomeBtn.addEventListener('click', () => {
+
+        if(tome.classList.contains('read')) {
+        tome.classList.remove('read');
+        } else {
+        tome.classList.add('read');
+        };
+        
+    });
+
 
 };
